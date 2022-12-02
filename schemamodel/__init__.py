dc08da65b00ce8448c3038e65e89b45b3a001158
Jurnell Cockhren <jurnell@civichacker.com>
2022-12-02 12:26:53 -0600

    alpha version ready

---
+++ schemamodel/__init__.py
@@ -0,0 +1,58 @@
+from abc import ABC, abstractmethod
+from dataclasses import make_dataclass, field
+from re import sub
+import importlib
+
+from schemamodel import dynamic
+
+
+JSON_TYPE_MAP = {
+    'string': str,
+    'integer': int,
+    'number': float,
+    'null': None
+}
+
+class Exporter(ABC):
+    @abstractmethod
+    def export(self): pass
+
+    @classmethod
+    def __subclasshook__(cls, C):
+        if cls is MyIterable:
+            if any("__iter__" in B.__dict__ for B in C.__mro__):
+                return True
+        return NotImplemented
+
+
+def generate_classname(title: str) -> str:
+    return sub(r'(-|_)+', '', title.title())
+
+
+class SchemaModel:
+    def __init__(self, schemas=[], allow_remote=False):
+        self.dmod = importlib.import_module('schemamodel.dynamic')
+
+    def register(self, schema: dict) -> bool:
+        if schema.get('type', None) != 'object':
+            return False
+        fields = list()
+        fields_with_defaults = list()
+        required_fields = schema.get('required', [])
+        for k,v in schema['properties'].items():
+            entry = (k, JSON_TYPE_MAP.get(v.get('type')))
+            if len(required_fields) > 0 and k not in required_fields:
+                entry += (field(default=None), )
+                fields_with_defaults.append(entry)
+            elif'default' in v.keys():
+                entry += (field(default=v.get('default')), )
+                fields_with_defaults.append(entry)
+            else:
+                fields.append(entry)
+        C = make_dataclass(
+            generate_classname(schema.get('title')),
+            fields + fields_with_defaults,
+            frozen=True
+        )
+        setattr(self.dmod, generate_classname(schema.get('title')), C)
+        return True
---
+++ tests.py
@@ -0,0 +1,105 @@
+from jsonschema import validators, protocols, validate
+import json
+import importlib
+from dataclasses import make_dataclass, FrozenInstanceError, asdict
+
+from schemamodel import SchemaModel
+
+import pytest
+
+def test_enforce_required():
+    test = '''
+    {
+        "$id": "https://schema.dev/fake-schema.schema.json",
+        "$schema": "http://json-schema.org/draft-07/schema#",
+        "title": "fake-schema",
+        "description": "",
+        "type": "object",
+        "properties": {
+            "provider_id": {
+              "type": "integer"
+            },
+            "brand_name": {
+              "type": "string"
+            }
+        },
+        "required": ["brand_name"]
+    }
+    '''
+    t = json.loads(test)
+    dmod = importlib.import_module('schemamodel.dynamic')
+    sm = SchemaModel()
+    validators.Draft202012Validator.check_schema(t)
+
+    try:
+        assert sm.register(t)
+        from schemamodel.dynamic import FakeSchema
+    except Exception:
+        assert False
+
+    with pytest.raises(TypeError):
+        FakeSchema()
+        FakeSchema(provider_id=1)
+
+    assert validate(asdict(FakeSchema(brand_name="yo", provider_id=3)), t) is None
+
+
+def test_immutability():
+    test = '''
+    {
+        "$id": "https://schema.dev/fake-schema.schema.json",
+        "$schema": "http://json-schema.org/draft-07/schema#",
+        "title": "fake-schema",
+        "description": "Blue Blah",
+        "type": "object",
+        "properties": {
+            "provider_id": {
+              "type": "integer"
+            },
+            "brand_name": {
+              "type": "string"
+            }
+        }
+    }
+    '''
+    t = json.loads(test)
+    sm = SchemaModel()
+    sm.register(t)
+
+    from schemamodel.dynamic import FakeSchema
+    fs = FakeSchema(provider_id=1, brand_name="yo")
+
+    assert validate(asdict(FakeSchema(brand_name="yo", provider_id=3)), t) is None
+    with pytest.raises(FrozenInstanceError):
+        fs.provider_id = 3
+
+
+def test_default_support():
+    test = '''
+    {
+        "$id": "https://schema.dev/fake-schema.schema.json",
+        "$schema": "http://json-schema.org/draft-07/schema#",
+        "title": "fake-schema",
+        "description": "Blue Blah",
+        "type": "object",
+        "properties": {
+            "provider_id": {
+              "default": 5,
+              "type": "integer"
+            },
+            "brand_name": {
+              "type": "string"
+            }
+        }
+    }
+    '''
+    t = json.loads(test)
+    sm = SchemaModel()
+    sm.register(t)
+
+    from schemamodel.dynamic import FakeSchema
+    fs = FakeSchema(brand_name="yo")
+    assert validate({}, t) is None
+
+    assert fs.provider_id == 5
+
