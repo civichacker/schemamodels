9103adafaf38980a5deb07b7003aabbd52b7dabf
Jurnell Cockhren <jurnell@civichacker.com>
2025-05-18 03:29:06 -0500

    new suite of custom Field descriptors to handle basic type checks in the Dataclasses this library generates.

--- schemamodels/bases.py
+++ schemamodels/bases.py
@@ -4,7 +4,9 @@
 from abc import ABC, abstractmethod
 from collections import deque, UserList
 from functools import partial, reduce
-from typing import Callable
+from typing import Callable, Protocol, TypeVar, Generic, AnyStr, get_args
+from schemamodels import exceptions as e
+from operator import contains, le, ge, gt, lt, mod, not_
 
 
 class BaseErrorHandler(ABC):
@@ -35,6 +37,99 @@ class BaseRenderer(ABC):
             if "apply" in klass.__dict__:
                 return True
         return NotImplementedError()
+
+
+A = TypeVar('A')
+T = TypeVar('T', bound=str)
+N = TypeVar('N', int, float)
+S = TypeVar('S', bound=None)
+C = TypeVar('C', list, tuple)
+O = TypeVar('O', bound=dict)
+B = TypeVar('B', bound=bool)
+
+
+class JSONSchemaFieldDescriptor(Protocol[A]):
+
+    def __init__(self, *, default):
+        self._default = default
+
+
+    def __get__(self, obj, type):
+        if obj is None:
+            return self._default
+
+        return self.__dict__.get(self._name, self._default)
+
+
+    def __set__(self, obj, value):
+        ttype = get_args(self.__orig_bases__[0])[0]
+
+        if not isinstance(value, ttype.__constraints__):
+            raise e.ValueTypeViolation()
+
+
+class ArrayDescriptor(Generic[C]): pass
+class BooleanDescriptor(JSONSchemaFieldDescriptor[S]): pass
+class ObjectDescriptor(Generic[O]): pass
+
+class StringDescriptor(JSONSchemaFieldDescriptor[AnyStr]):
+    COMPARISONS = {
+        'enum': {'comparator': lambda d: partial(contains, d), 'errorClass': e.ValueTypeViolation},
+        'maxLength': {'comparator': lambda d: partial(lambda bound, v: len(v) <= bound, d), 'errorClass': e.LengthConstraintViolation},
+        'not': {'comparator': lambda d: not isinstance(d, str), 'errorClass': e.ValueTypeViolation },
+        'minLength': {'comparator': lambda d: partial(lambda bound, v: len(v) >= bound, d), 'errorClass': e.LengthConstraintViolation}
+    }
+
+    def __init__(self, *, default=None, metadata={}):
+        self._metadata = metadata
+        self._default = default
+
+    def __set_name__(self, o, name):
+        self._name = name
+
+    def __set__(self, obj, value):
+        super().__set__(obj, value)
+
+        for metakey, metaval in self._metadata.items():
+            key = self.COMPARISONS.get(metakey, None)
+            if not key:
+                raise Exception(f"Comparison keyword {metakey}:{key} is not supported")
+            if not key.get('comparator')(metaval)(value):
+                raise key.get('errorClass')(f'{self._name}={value} =/= {metakey}={metaval} constraint fails')
+
+        self.__dict__[self._name] = value
+
+
+class NumberDescriptor(JSONSchemaFieldDescriptor[N]):
+
+    COMPARISONS = {
+        'minimum': {'comparator': lambda d: partial(le, d), 'errorClass': e.ValueTypeViolation},
+        'maximum': {'comparator': lambda d: partial(ge, d), 'errorClass': e.RangeConstraintViolation},
+        'exclusiveMinimum': {'comparator': lambda d: partial(lt, d), 'errorClass': e.RangeConstraintViolation},
+        'exclusiveMaximum': {'comparator': lambda d: partial(gt, d), 'errorClass': e.RangeConstraintViolation},
+        'multipleOf': {'comparator': lambda d: partial(lambda d, n: mod(n, d) == 0, d), 'errorClass': e.RangeConstraintViolation},
+        'not': {'comparator': lambda d: not_(d), 'errorClass': e.ValueTypeViolation },
+    }
+
+    # All fields support default arg
+    def __init__(self, *, default=None, metadata={}):
+        self._metadata = metadata
+        self._default = default
+
+    def __set_name__(self, o, name):
+        self._name = name
+
+    def __set__(self, obj, value):
+        super().__set__(obj, value)
+
+        for metakey, metaval in self._metadata.items():
+            key = self.COMPARISONS.get(metakey, None)
+            if not key:
+                raise Exception(f"Comparison keyword {metakey}:{key} is not supported")
+            if not key.get('comparator')(metaval)(value):
+                raise key.get('errorClass')(f'{self._name}={value} =/= {metakey}={metaval} constraint fails')
+
+        self.__dict__[self._name] = value
 
 
 class CoreModel(dict):
